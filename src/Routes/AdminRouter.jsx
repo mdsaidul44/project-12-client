1ebb019844fc4a1d2af91b496097b8e575d657c3
mdsaidul44 <islam88758@gmail.com>
2024-06-06 21:15:17 +0600

    admin router setup done

---
+++ src/Routes/AdminRouter.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+
+ 
+
+const AdminRouter = ({children}) => {
+    const {user,loading} =useAuth()
+    const location = useLocation()
+     
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }if(user){
+        return children
+    }
+    return  <Navigate to='/' state={{from:location}}></Navigate>
+};
+
+export default AdminRouter;
