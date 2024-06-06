a4af72a64f728721bd04726b934b0918e41f1670
mdsaidul44 <islam88758@gmail.com>
2024-06-06 21:09:16 +0600

    private route done

---
+++ src/Routes/PrivateRouter.jsx
@@ -0,0 +1,17 @@
+import { useLocation } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+
+ 
+
+const PrivateRouter = ({children}) => {
+    const {user , loading} = useAuth()
+    const location = useLocation()
+    if(loading){
+        return <progress className="progress w-56"></progress>
+    }if(user){
+        return children
+    }
+    return <Navigate to='/login' state={{from:location}}></Navigate>
+};
+
+export default PrivateRouter;
